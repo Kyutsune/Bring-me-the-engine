13dd92dc3c0a281b289fb906b80923c69285067b
p2100189 <romain.soares@etu.univ-lyon1.fr>
2025-07-09 20:18:16 +0200

    Test entier de Vec3

--- tests/test_vec.cpp
+++ tests/test_vec.cpp
@@ -4,6 +4,13 @@ using namespace Catch;
 
 #include "math/Vec.h"
 
+TEST_CASE("Création de Vec3", "[vec3]") {
+    Vec3 v(1, 2, 3);
+    REQUIRE(v.x == Approx(1.0f));
+    REQUIRE(v.y == Approx(2.0f));
+    REQUIRE(v.z == Approx(3.0f));
+}
+
 TEST_CASE("Addition de Vec3", "[vec3]") {
     Vec3 a(1, 2, 3);
     Vec3 b(4, 5, 6);
@@ -63,4 +70,49 @@ TEST_CASE("Multiplication par un scalaire négatif", "[vec3]") {
     REQUIRE(b.x == Approx(-2.0f));
     REQUIRE(b.y == Approx(-4.0f));
     REQUIRE(b.z == Approx(-6.0f));
+}
+
+TEST_CASE("Multiplication par un autre Vec3", "[vec3]") {
+    Vec3 a(1, 2, 3);
+    Vec3 b(4, 5, 6);
+    Vec3 c = a * b;
+
+    REQUIRE(c.x == Approx(4.0f));
+    REQUIRE(c.y == Approx(10.0f));
+    REQUIRE(c.z == Approx(18.0f));
+}
+
+TEST_CASE("Normalisation de Vec3", "[vec3]") {
+    Vec3 a(3, 4, 0);
+    Vec3 b = a.normalized();
+
+    REQUIRE(b.length() == Approx(1.0f));
+    REQUIRE(b.x == Approx(0.6f));
+    REQUIRE(b.y == Approx(0.8f));
+    REQUIRE(b.z == Approx(0.0f));
+}
+
+TEST_CASE("Longueur de Vec3", "[vec3]") {
+    Vec3 a(3, 4, 0);
+    float length = a.length();
+
+    REQUIRE(length == Approx(5.0f));
+}
+
+TEST_CASE("produit scalaire de Vec3", "[vec3]") {
+    Vec3 a(1, 2, 3);
+    Vec3 b(4, 5, 6);
+    float dotProduct = a.dot(b);
+
+    REQUIRE(dotProduct == Approx(32.0f));
+}
+
+TEST_CASE("produit vectoriel de Vec3", "[vec3]") {
+    Vec3 a(1, 2, 3);
+    Vec3 b(4, 5, 6);
+    Vec3 crossProduct = a.cross(b);
+
+    REQUIRE(crossProduct.x == Approx(-3.0f));
+    REQUIRE(crossProduct.y == Approx(6.0f));
+    REQUIRE(crossProduct.z == Approx(-3.0f));
 }
