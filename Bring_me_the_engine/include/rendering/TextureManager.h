b9ebdd2265b377ef17e8567bc3f3b3e663adc578
Kyutsune <jaipechofizz@gmail.com>
2025-07-10 18:29:10 +0200

    Rajout d'un AVENIR pour se souvenir du soucis du texturemanager

--- Bring_me_the_engine/include/rendering/TextureManager.h
+++ Bring_me_the_engine/include/rendering/TextureManager.h
@@ -4,6 +4,10 @@
 #include <memory>
 #include "rendering/Texture.h"
 
+//AVENIR: Dans un futur hypothétique ou Bring me the engine se développe plus, le fait de garder en cache
+// Toutes les textures tout le temps peut devenir problématique en termes de mémoire.
+// Il serait peut-être judicieux de mettre en place un système de gestion de mémoire plus sophistiqué,
+// comme un système de chargement/déchargement dynamique des textures en fonction de leur utilisation.
 class TextureManager {
 public:
     static std::shared_ptr<Texture> load(const std::string& path);
